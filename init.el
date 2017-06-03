a451d2856cf7ddde7b7cd2334323f06906b18f9e
Shahin <me@5hah.in>
2017-06-03 16:22:10 +0430

    Add some windowing keybindings

--- init.el
+++ init.el
@@ -23,7 +23,10 @@
 ;; default-keybindings
 (evil-leader/set-key
   "ff" 'find-file
-  "fs" 'save-buffer)
+  "fs" 'save-buffer
+  "w/" 'split-window-right
+  "w-" 'split-window-below
+  "wd" 'delete-window)
 
 ;; avy
 ; http://emacsredux.com/blog/2015/07/19/ace-jump-mode-is-dead-long-live-avy/
@@ -37,7 +40,11 @@
   :config
   (evil-mode +1)
   (evil-leader/set-key
-    "gs" 'magit-status))
+    "gs" 'magit-status
+    "wh" 'evil-window-left
+    "wj" 'evil-window-down
+    "wl" 'evil-window-right
+    "wk" 'evil-window-up))
 
 ;; magit
 (use-package magit)
@@ -52,7 +59,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (avy evil-magit evil-leader magit use-package evil ace-jump-mode))))
+    (evil-window ace-window avy evil-magit evil-leader magit use-package evil ace-jump-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
