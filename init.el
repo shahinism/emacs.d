d5100a18ce4341ce31ef121012000171226e5202
Shahin <me@5hah.in>
2017-06-04 14:29:30 +0430

    Add kill current buffer function and bind it to bd

--- init.el
+++ init.el
@@ -23,12 +23,18 @@
   (evil-leader/set-leader "<SPC>"))
 
 ;; default-keybindings
+(defun kill-current-buffer ()
+  "Kill current buffer"
+  (interactive)
+  (kill-buffer (current-buffer)))
+
 (evil-leader/set-key
   ;;"ff" 'find-file
   "fs" 'save-buffer
   "w/" 'split-window-right
   "w-" 'split-window-below
-  "wd" 'delete-window)
+  "wd" 'delete-window
+  "bd" 'kill-current-buffer)
 
 ;; default variables
 (setq-default
