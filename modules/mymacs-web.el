aeff28fef3681504535f1f636e9989ff3bd63431
Shahin <me@5hah.in>
2017-06-05 21:00:43 +0430

    Add web-mode for web pages

---
+++ modules/mymacs-web.el
@@ -0,0 +1,30 @@
+;; web-mode
+(use-package web-mode
+  :config
+  (setq web-mode-markup-indent-offset 2)
+
+  (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode)))
+
+; make web-mode play nice with smartparens
+; https://github.com/bbatsov/prelude/blob/master/modules/prelude-web.el
+(setq web-mode-enable-auto-pairing nil)
+
+(sp-with-modes '(web-mode)
+  (sp-local-pair "%" "%"
+                 :unless '(sp-in-string-p)
+                 :post-handlers '(((lambda (&rest _ignored)
+                                     (just-one-space)
+                                     (save-excursion (insert " ")))
+                                   "SPC" "=" "#")))
+  (sp-local-tag "%" "<% "  " %>")
+  (sp-local-tag "=" "<%= " " %>")
+  (sp-local-tag "#" "<%# " " %>"))
+
+(provide 'mymacs-web)
