2208204bd52c8d6fdb9900eea276e41bd844c021
Shahin <me@5hah.in>
2017-06-05 18:54:25 +0430

    Add pip-requirements mode

--- modules/mymacs-python.el
+++ modules/mymacs-python.el
@@ -1,4 +1,9 @@
 ;; python
+(use-package python
+  :init
+  (setq python-indent-guess-indent-offset-verbose nil
+        python-shell-interpreter "python"))
+
 (defun python-mode-defaults ()
   "Default configuration for python mode"
   (subword-mode +1)
@@ -6,6 +11,7 @@
   (eldoc-mode 1)
   (add-hook 'python-mode-hook 'anaconda-eldoc-mode))
 
+;; anaconda
 (use-package anaconda-mode
   :config
   (add-hook 'python-mode-hook 'python-mode-defaults))
@@ -16,5 +22,10 @@
   company
   :config
   (add-to-list 'company-backends 'company-anaconda))
+
+;; pip-requirements
+; https://github.com/Wilfred/pip-requirements.el
+(use-package pip-requirements
+  :mode ("/requirements.txt$" . pip-requirements-mode))
 
 (provide 'mymacs-python)
