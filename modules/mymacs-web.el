079b0c66e370b2004ab00a133c103c28b3110038
Shahin <me@5hah.in>
2017-06-05 22:36:05 +0430

    Use use-package to define webmode file types

--- modules/mymacs-web.el
+++ modules/mymacs-web.el
@@ -1,16 +1,15 @@
 ;; web-mode
 (use-package web-mode
+  :mode ("\\.p?html?$"
+         "\\.\\(tpl\\|blade\\)\\(\\.php\\)?$"
+         "\\.erb$"
+         "\\.jsp$"
+         "\\.as[cp]x$"
+         "\\.mustache$"
+         "\\.tsx$"
+         "wp-content/themes/.+/.+\\.php$")
   :config
-  (setq web-mode-markup-indent-offset 2)
-
-  (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode)))
+  (setq web-mode-markup-indent-offset 2))
 
 ; make web-mode play nice with smartparens
 ; https://github.com/bbatsov/prelude/blob/master/modules/prelude-web.el
