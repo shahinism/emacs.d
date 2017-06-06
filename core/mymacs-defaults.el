a4e964a8bc3b5d97b4ef80905eb33abe66a28872
Shahin <me@5hah.in>
2017-06-06 12:32:12 +0430

    Clearup modeline a little bit

--- core/mymacs-defaults.el
+++ core/mymacs-defaults.el
@@ -4,14 +4,6 @@
 (setq auto-save-file-name-transforms
     `((".*" ,temporary-file-directory t)))
 
-;; smooth scrolling
-; scroll one line at a time (less "jumpy" than defaults)
-; https://www.emacswiki.org/emacs/SmoothScrolling
-(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)) ;; one line at a time
-      mouse-wheel-progressive-speed nil            ;; don't accelerate scrolling
-      mouse-wheel-follow-mouse 't                  ;; scroll window under mouse
-      scroll-step 1)                               ;; keyboard scroll one line at a time
-
 ;; save place
 (save-place-local-mode 1)
 
--- core/mymacs-ui.el
+++ core/mymacs-ui.el
@@ -1,19 +1,39 @@
 ;; default variables
-(setq-default
- fill-column 80
- visible-bell t
- initial-scratch-message ""
- inhibit-startup-message t
- ; http://ergoemacs.org/emacs/emacs_tabs_space_indentation_setup.html
- indent-tabs-mode nil
- tab-width 4)
+(setq-default fill-column 80
+              visible-bell t
+              initial-scratch-message ""
+              inhibit-startup-message t
+              ; http://ergoemacs.org/emacs/emacs_tabs_space_indentation_setup.html
+              indent-tabs-mode nil
+              tab-width 4)
 
+;; smooth scrolling
+; scroll one line at a time (less "jumpy" than defaults)
+; https://www.emacswiki.org/emacs/SmoothScrolling
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)) ;; one line at a time
+      mouse-wheel-progressive-speed nil            ;; don't accelerate scrolling
+      mouse-wheel-follow-mouse 't                  ;; scroll window under mouse
+      scroll-step 1                                ;; keyboard scroll one line at a time
+      ;; nice scrolling
+      scroll-margin 0
+      scroll-conservatively 100000
+      scroll-preserve-screen-position 1)
+
+;; strip emacs
 ; https://emacs-doctor.com/emacs-strip-tease.html
 (scroll-bar-mode 0)
 (tool-bar-mode 0)
 (menu-bar-mode 0)
 (global-linum-mode 1)
 (global-hl-line-mode 1)
+
+;; mode line
+(line-number-mode t)
+(column-number-mode t)
+(size-indication-mode t)
+
+;; y/n answers
+(fset 'yes-or-no-p 'y-or-n-p)
 
 ;; enable undo-tree globally
 (global-undo-tree-mode)
@@ -22,5 +42,13 @@
 (use-package volatile-highlights
   :config
   (volatile-highlights-mode t))
+
+;; smart-mode-line
+(use-package smart-mode-line
+  :config
+  (setq  sml/no-confirm-load-theme t
+         ; delegate theming to the currently active theme
+         sml/theme nil)
+  (add-hook 'after-init-hook #'sml/setup))
 
 (provide 'mymacs-ui)
