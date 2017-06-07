e704080a1b813347586cd9eb0d408878b85cfcc6
Shahin <me@5hah.in>
2017-06-07 15:55:30 +0430

    Add a basic compile function to compile all .el files

---
+++ core/mymacs-utils.el
@@ -0,0 +1,15 @@
+(defun mymacs-compile ()
+  (let ((targets (append (list (expand-file-name "init.el" user-emacs-directory))
+                         (directory-files mymacs-core-dir t "^[^\.].+\.el$")
+                         (directory-files mymacs-modules-dir t "^[^\.].+\.el$"))))
+    (mapc (lambda (file)
+            (let ((result (byte-compile-file file))
+                  (short-name (file-relative-name file user-emacs-directory)))
+              (cond ((eq result 'no-byte-compile)
+                     (message "Ignored %s" short-name))
+                    ((null result)
+                     (message "Failed to compile %s" short-name))
+                    (t
+                     (message "Compiled %s" short-name))))) targets)))
+
+(provide 'mymacs-utils)
