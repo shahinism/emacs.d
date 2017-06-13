45c691cd69610553a74dc377af03e36f84dd2d03
Shahin <me@5hah.in>
2017-06-13 19:55:39 +0430

    Fix rust mode problems

--- modules/mymacs-rust.el
+++ modules/mymacs-rust.el
@@ -3,21 +3,51 @@
   (interactive)
   (setq-local helm-dash-docsets '("Rust")))
 
+;; rust-mode
 (use-package rust-mode
   :mode "\\.rs$"
   :init
-  (add-hook 'rust-mode-hook #'flycheck-mode))
+  (add-hook 'rust-mode-hook #'flycheck-mode)
+  (evil-leader/set-key-for-mode 'rust-mode
+    "=" 'rust-format-buffer))
 
+;; flycheck-rust
 (use-package flycheck-rust
-  :after rust-mode)
+  :after rust-mode
+  :config
+  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
 
+;; toml-mode
+(use-package toml-mode
+  :mode "/\\(Cargo.lock\\|\\.cargo/config\\)\\'")
+
+;; cargo
+(use-package cargo
+  :config
+  (add-hook 'rust-mode-hook 'cargo-minor-mode)
+  (evil-leader/set-key-for-mode 'rust-mode
+        "c." 'cargo-process-repeat
+        "cC" 'cargo-process-clean
+        "cX" 'cargo-process-run-example
+        "cc" 'cargo-process-build
+        "cd" 'cargo-process-doc
+        "ce" 'cargo-process-bench
+        "cf" 'cargo-process-current-test
+        "cf" 'cargo-process-fmt
+        "ci" 'cargo-process-init
+        "cn" 'cargo-process-new
+        "co" 'cargo-process-current-file-tests
+        "cs" 'cargo-process-search
+        "cu" 'cargo-process-update
+        "cx" 'cargo-process-run
+        "t" 'cargo-process-test))
+
+;; racer
 (use-package racer
   :after rust-mode
-  :init
-  (add-hook 'rust-mode-hook #'rust-doc)
+  :config
   (add-hook 'rust-mode-hook #'racer-mode)
-  (add-hook 'rust-mode-hook #'eldoc-mode)
-  (add-hook 'rust-mode-hook #'flycheck-rust-setup)
+  (add-hook 'racer-mode-hook #'eldoc-mode)
   (add-hook 'racer-mode-hook #'company-mode))
 
 (provide 'mymacs-rust)
