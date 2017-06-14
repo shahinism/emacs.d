10631fc47df45e556936b616ac93b1f4f19a19a5
Shahin <me@5hah.in>
2017-06-14 20:57:09 +0430

    Add basic C support

---
+++ modules/mymacs-c.el
@@ -0,0 +1,35 @@
+;; cc-mode
+(use-package cc-mode
+  :commands (c-mode c++-mode objc-mode java-mode)
+  :mode ("\\.mm" . objc-mode))
+
+;; irony
+(use-package irony
+  :after cc-mode
+  :config
+  (add-hook 'c++-mode-hook 'irony-mode)
+  (add-hook 'c-mode-hook 'irony-mode)
+  (add-hook 'objc-mode-hook 'irony-mode)
+
+  (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options))
+
+(use-package company-irony
+  :after irony
+  :after company
+  :config
+  (add-to-list 'company-backends 'company-irony))
+
+;; flycheck-irony
+(use-package flycheck-irony
+  :after irony
+  :after flycheck
+  :config
+  (add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
+
+;; disaster
+(use-package disaster
+  :config
+  (evil-leader/set-key-for-mode 'c-mode
+    "cd" 'disaster))
+
+(provide 'mymacs-c)
