3832d80935df14ef7d20c1b947d44571ce38cef1
Shahin <me@5hah.in>
2017-06-24 18:05:26 +0430

    Add a basic support for clojure-script

--- init.el
+++ init.el
@@ -12,7 +12,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (web-beautify flycheck-flow flycheck-mypy doom-themes esup zenburn-theme yapfify yaml-mode xref-js2 which-key web-mode volatile-highlights use-package toml-mode spaceline smartparens rjsx-mode rainbow-mode rainbow-delimiters racer pyvenv pyenv-mode py-isort pip-requirements pdf-tools org-plus-contrib org-bullets neotree json-mode helm-projectile helm-dash haml-mode gitignore-mode gitconfig-mode fringe-helper flycheck-rust flycheck-pos-tip flycheck-irony exec-path-from-shell evil-org evil-nerd-commenter evil-matchit evil-magit evil-leader evil-iedit-state emojify dockerfile-mode disaster company-web company-tern company-quickhelp company-irony company-auctex company-anaconda coffee-mode clang-format cargo beacon avy all-the-icons))))
+    (clojure-mode ereader web-beautify flycheck-flow flycheck-mypy doom-themes esup zenburn-theme yapfify yaml-mode xref-js2 which-key web-mode volatile-highlights use-package toml-mode spaceline smartparens rjsx-mode rainbow-mode rainbow-delimiters racer pyvenv pyenv-mode py-isort pip-requirements pdf-tools org-plus-contrib org-bullets neotree json-mode helm-projectile helm-dash haml-mode gitignore-mode gitconfig-mode fringe-helper flycheck-rust flycheck-pos-tip flycheck-irony exec-path-from-shell evil-org evil-nerd-commenter evil-matchit evil-magit evil-leader evil-iedit-state emojify dockerfile-mode disaster company-web company-tern company-quickhelp company-irony company-auctex company-anaconda coffee-mode clang-format cargo beacon avy all-the-icons))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
