7877b792a94db80a9f2707cd2fe75ecc1bce83c2
Shahin <me@5hah.in>
2017-06-28 08:34:56 +0430

    Add some basic keybindings to handle org easier

--- init.el
+++ init.el
@@ -10,6 +10,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/Work/Dropbox/notes/org/journal.org")))
  '(package-selected-packages
    (quote
     (helm-c-yasnippet clojure-mode ereader web-beautify flycheck-flow flycheck-mypy doom-themes esup zenburn-theme yapfify yaml-mode xref-js2 which-key web-mode volatile-highlights use-package toml-mode spaceline smartparens rjsx-mode rainbow-mode rainbow-delimiters racer pyvenv pyenv-mode py-isort pip-requirements pdf-tools org-plus-contrib org-bullets neotree json-mode helm-projectile helm-dash haml-mode gitignore-mode gitconfig-mode fringe-helper flycheck-rust flycheck-pos-tip flycheck-irony exec-path-from-shell evil-org evil-nerd-commenter evil-matchit evil-magit evil-leader evil-iedit-state emojify dockerfile-mode disaster company-web company-tern company-quickhelp company-irony company-auctex company-anaconda coffee-mode clang-format cargo beacon avy all-the-icons))))
