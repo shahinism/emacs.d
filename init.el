a90961c5e0cb38ac0dff3b980432b34181f08a45
Shahin <me@5hah.in>
2017-07-01 00:22:13 +0430

    Config telephone-mode-line

--- init.el
+++ init.el
@@ -5,3 +5,17 @@
 ;; (package-initialize)
 
 (org-babel-load-file "~/.emacs.d/configs.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yapfify yaml-mode xref-js2 which-key web-mode web-beautify volatile-highlights use-package toml-mode telephone-line smartparens rjsx-mode rainbow-mode rainbow-delimiters racer pyvenv pyenv-mode py-isort pip-requirements pig-mode pdf-tools ox-twbs org-bullets neotree moe-theme mode-icons json-mode iedit htmlize helm-projectile helm-dash helm-c-yasnippet haml-mode graphviz-dot-mode gitignore-mode gitconfig-mode fringe-helper flyspell-correct-helm flycheck-rust flycheck-pos-tip flycheck-irony exec-path-from-shell evil-org evil-nerd-commenter evil-matchit evil-magit evil-leader ereader emojify dockerfile-mode disaster csv-mode company-web company-tern company-irony company-auctex company-anaconda coffee-mode clojure-mode clang-format cargo beacon avy all-the-icons))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
