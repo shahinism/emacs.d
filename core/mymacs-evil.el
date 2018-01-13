3e80acff3839ceffb42711b10bc723b576edb3e0
Shahin <me@5hah.in>
2018-01-13 20:10:45 +0330

    Enable evil collection only on info mode for now

--- core/mymacs-evil.el
+++ core/mymacs-evil.el
@@ -1,21 +1,35 @@
-;; evil-leader
-; https://github.com/cofi/evil-leader
-(use-package evil-leader
-  :config
-  (global-evil-leader-mode)
-  (evil-leader/set-leader "<SPC>"))
-
 ;; evil
 ; https://github.com/emacs-evil/evil
 (use-package evil
+  :ensure t
+  :init
+  (setq evil-want-integration nil)
   :config
-  (evil-mode +1)
+  (evil-mode 1)
   (evil-leader/set-key
     "gs" 'magit-status
     "wh" 'evil-window-left
     "wj" 'evil-window-down
     "wl" 'evil-window-right
     "wk" 'evil-window-up))
+
+;; https://github.com/jojojames/evil-collection
+(use-package evil-collection
+  :after evil
+  :ensure t
+  :init
+  (progn
+    (setq evil-collection-mode-list '(info)))
+  :config
+  (evil-collection-init))
+
+;; evil-leader
+; https://github.com/cofi/evil-leader
+(use-package evil-leader
+  :after evil
+  :config
+  (global-evil-leader-mode)
+  (evil-leader/set-leader "<SPC>"))
 
 ;; which-key
 (use-package which-key
