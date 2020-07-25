200e5e3fefdce5415667a61849c4b812f6f1b19f
Shahin <me@5hah.in>
2020-07-25 18:37:56 +0430

    Configure evil, general and some hydras.

--- init.el
+++ init.el
@@ -6,7 +6,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(doom-themes magit evil use-package)))
+ '(package-selected-packages
+   '(evil-magit hydra use-package-hydra general evil-collection ivy-rich restart-emacs counsel ivy-hydra ivy doom-themes magit evil use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
