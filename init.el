fa4ceaa4eae8f70300a06e539db85b703884bfda
Shahin <me@5hah.in>
2020-08-15 19:09:28 +0430

    add ace-jump

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(modus-operandi-theme smartparens hydra-examples ace-window evil-magit hydra use-package-hydra general evil-collection ivy-rich restart-emacs counsel ivy-hydra ivy doom-themes magit evil use-package)))
+   '(ace-jump-mode modus-operandi-theme smartparens hydra-examples ace-window evil-magit hydra use-package-hydra general evil-collection ivy-rich restart-emacs counsel ivy-hydra ivy doom-themes magit evil use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
