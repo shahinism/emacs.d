86bb9d28ed2f13ca4c856b5a501f8b88ce3f6bda
Shahin <me@5hah.in>
2021-02-28 12:27:27 +0100

    add some org config.

--- core/mymacs-org.el
+++ core/mymacs-org.el
@@ -1,3 +1,8 @@
+(use-package org-bullets
+  :straight t
+  :config
+  (add-hook 'org-mode-hook #'org-bullets-mode))
+
 (use-package org
   ;; :straight org-plus-contrib
   :config
@@ -17,7 +22,12 @@
   (add-to-list 'org-structure-template-alist '("ts" . "src typescript"))
   (add-to-list 'org-structure-template-alist '("py" . "src python"))
   (add-to-list 'org-structure-template-alist '("yaml" . "src yaml"))
-  (add-to-list 'org-structure-template-alist '("json" . "src json"))    
+  (add-to-list 'org-structure-template-alist '("json" . "src json"))
+
+  (org-babel-do-load-languages 'org-babel-load-languages
+                               '((shell . t)
+                                 (python . t)
+                                 (emacs-lisp . t)))
 
   (setq org-todo-keywords '((sequence "[ ](t)" "[w](w)" "|" "[x](d)" "[-](c)")))
   (require 'org-protocol))
@@ -50,6 +60,29 @@
          :head "#+title: %<%Y-%m-%d>\n"
          :olp ("Technical"))))
   )
+
+(use-package doct
+  :straight t
+  :after org
+  :config
+
+  (setq org-capture-templates
+        (doct `(
+                ("🌎 Webpage As Entry"
+                 :keys "w"
+                 :type plain
+                 :file "~/org/inbox.org"
+                 :prepend t
+                 :empty-lines-after 2
+                 :headline "Bookmarks"
+                 :template "** "
+                 :hook ,(defun my/website-to-org-entry ()
+                          "Convert clipboard's URL content to org entry."
+                          (org-web-tools-insert-web-page-as-entry (org-get-x-clipboard 'PRIMARY)))
+                 )))))
+
+(use-package org-web-tools
+  :straight t)
 
 
 (provide 'mymacs-org)
