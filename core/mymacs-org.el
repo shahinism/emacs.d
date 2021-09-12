e16e070e0462575030211e5bb9617116fb9a0ac6
Shahin <me@5hah.in>
2021-09-12 08:00:52 +0200

    add some updates.

--- core/mymacs-org.el
+++ core/mymacs-org.el
@@ -52,6 +52,36 @@
                           (org-web-tools-insert-web-page-as-entry (org-get-x-clipboard 'PRIMARY)))
                  )))))
 
+
+(use-package org-roam
+      :straight t
+      :custom
+      (org-roam-directory (file-truename "~/org/roam"))
+      ;; :bind (("C-c n l" . org-roam-buffer-toggle)
+      ;;        ("C-c n f" . org-roam-node-find)
+      ;;        ("C-c n g" . org-roam-graph)
+      ;;        ("C-c n i" . org-roam-node-insert)
+      ;;        ("C-c n c" . org-roam-capture)
+      ;;        ;; Dailies
+      ;;        ("C-c n j" . org-roam-dailies-capture-today))
+      :init
+      (setq org-roam-v2-ack t)
+      :config
+      (sa/leader-key-def
+        "r" '(:ignore t :which-key "roam")
+        "rl" 'org-roam-buffer-toggle
+        "rf" 'org-roam-node-find
+        "rg" 'org-roam-graph
+        "ri" 'org-roam-node-insert
+        "rc" 'org-roam-capture
+        "rd" '(:ignore t :which-key "dailies")
+        "rdt" 'org-roam-dailies-capture-today)
+
+      (org-roam-setup)
+      ;; disable v1->v2 migration warning
+      ;; If using org-roam-protocol
+      (require 'org-roam-protocol))
+
 ;;(use-package org-web-tools
 ;;  :straight t)
 
