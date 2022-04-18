e4ae0df8493a32af06cc0f392f4d5c0beb93e575
Shahin <me@5hah.in>
2022-04-18 13:08:58 +0200

    refactor: switch theme.

--- init.el
+++ init.el
@@ -6,6 +6,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))
  '(package-selected-packages
    '(ace-jump-mode modus-operandi-theme smartparens hydra-examples ace-window evil-magit hydra use-package-hydra general evil-collection ivy-rich restart-emacs counsel ivy-hydra ivy doom-themes magit evil use-package)))
 (custom-set-faces
